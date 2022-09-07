7bef0c06d4709fca76641785c4189fc00431af57
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-07 01:38:45 -0500

    Pass Lower Case

--- caesar-cypher.js
+++ caesar-cypher.js
@@ -1,6 +1,22 @@
 // 4. A caesarCipher function that takes a string and returns it with each
 //    character “shifted”. Read more about how a Caesar cipher works on this
 //    website.
-function caesarCipher(str) {}
+const ccRotate = (char, amount) => {
+    let offset = false;
+    if(/^[a-z]+$/.test(char)) offset = 'a'.charCodeAt(0);
+    if(/^[A-Z]+$/.test(char)) offset = 'A'.charCodeAt(0);
+    if(offset) {
+        const charCode = ((char.charCodeAt(0) - offset + amount) % 26) + offset;
+        console.log(charCode);
+        return String.fromCharCode(charCode);
+    }
+    return char;
+}
+
+const caesarCipher = (str, amount) =>
+  str
+    .split("")
+    .map((c) => ccRotate(c, amount))
+    .join("");
 
 module.exports = caesarCipher;
