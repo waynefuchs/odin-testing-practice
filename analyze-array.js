485b9613ce109a4e6dc3c6de869fd8438da9ab19
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-07 01:56:48 -0500

    Write test cases

---
+++ analyze-array.js
@@ -0,0 +1,7 @@
+// 5. An analyzeArray function that takes an array of numbers and returns
+//    an object with the following properties: average, min, max, and length.
+function analyzeArray(array) {
+
+}
+
+module.exports = analyzeArray;
--- analyze-array.test.js
+++ analyze-array.test.js
@@ -1,9 +1,10 @@
-const analyzeArray = require('./analyze-array');
+const analyzeArray = require("./analyze-array");
 
-// const object = analyzeArray([1,8,3,4,2,6]);
-// object == {
-//   average: 4,
-//   min: 1,
-//   max: 8,
-//   length: 6
-// };
+test("analyze array returns expected result", () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(object.average).toBeGreaterThan(4);
+  expect(object.average).toBeLessThan(5);
+  expect(object.min).toBe(1);
+  expect(object.max).toBe(8);
+  expect(object.length).toBe(6);
+});
