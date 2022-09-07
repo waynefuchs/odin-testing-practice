38a38d65fce8416c6eba67fef0d6303c1542e64f
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-07 00:32:46 -0500

    Add tests

--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,31 @@
-const calculator = require('./calculator');
+const calculator = require("./calculator");
 
-test('calculator adds two numbers correctly', () => {
-    const result = calculator.add(1,2);
-    expect(result).toBe(3);
-    expect(typeof(result)).toBe('number');
+test("calculator adds two numbers correctly", () => {
+  const result = calculator.add(1, 2);
+  expect(result).toBe(3);
+  expect(typeof result).toBe("number");
+});
+
+test("calculator subtracts two numbers correctly", () => {
+  const result = calculator.subtract(1, 2);
+  expect(result).toBe(-1);
+  expect(typeof result).toBe("number");
+});
+
+test("calculator divides two numbers into an integer correctly", () => {
+  const result = calculator.divide(10, 2);
+  expect(result).toBe(5);
+  expect(typeof result).toBe("number");
+});
+
+test("calculator divides two numbers into a floating point number correctly", () => {
+  const result = calculator.divide(1, 2);
+  expect(result).toBe(0.5);
+  expect(typeof result).toBe("number");
+});
+
+test("calculator multiplies a negative and positive number correctly", () => {
+  const result = calculator.multiply(-1, 13);
+  expect(result).toBe(-13);
+  expect(typeof result).toBe("number");
 });
