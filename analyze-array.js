a1706a1ab8e8bd979638768640d472997e6f5e31
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-07 01:57:04 -0500

    Pass expected result test

--- analyze-array.js
+++ analyze-array.js
@@ -1,7 +1,18 @@
 // 5. An analyzeArray function that takes an array of numbers and returns
 //    an object with the following properties: average, min, max, and length.
 function analyzeArray(array) {
-
+    return {
+        average: array.reduce((total, n) => (total += n), 0) / (array.length - 1),
+        min: array.reduce(
+          (min, n) => (min = n < min ? n : min),
+          Number.POSITIVE_INFINITY
+        ),
+        max: array.reduce(
+          (max, n) => (max = n > max ? n : max),
+          Number.NEGATIVE_INFINITY
+        ),
+        length: array.length,
+      };
 }
 
 module.exports = analyzeArray;
